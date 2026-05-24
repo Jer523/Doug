7b0da8f5059a94aa8aa9b8ce4da69701b6b62c85
Jerry <zetingyin@gmail.com>
2026-05-24 20:12:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ html, body,
 /* 脚注样式：延迟淡入、居中、可调大小字间距 */
 .footnote-streamlit {
     position: fixed;
-    bottom: 1.6rem;
-    left: 50%;
+    bottom: 0.6rem;
+    left: calc(50% + 8px);
     transform: translateX(-50%);
     font-size: 7.5px !important;
     color: #C0B8B0 !important;
