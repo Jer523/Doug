9c4ae82d8fa58037d0c03d27d207407039fd74de
Jerry <zetingyin@gmail.com>
2026-05-24 11:12:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ HTML = """
     animation:fadeUp 1.4s ease-out 2.5s both;
   }
   .divider {
-    width:255px;
+    width:257px;
     height:1px;
     background:#C8BFB0;
     margin:0 auto 2rem auto;
