783ef06874f7a75af986b08c89f563dec5078da3
Jerry <zetingyin@gmail.com>
2026-05-24 20:15:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ html, body,
 .footnote-streamlit {
     position: fixed;
     bottom: 0rem;
-    left: calc(50% + 88px);
+    left: calc(50% + 18px);
     transform: translateX(-50%);
     font-size: 7.5px !important;
     color: #C0B8B0 !important;
